623e3bdca38f7047cc13aff95778b15ba61195e1
gayoung <hanga93@gmail.com>
2023-02-20 17:54:08 +0900

    made buttons at HTML

--- calculator.js
+++ calculator.js
@@ -21,3 +21,19 @@ const divide = function (a,b){
     return a/b;
 }
 
+// create a new function called operate
+
+const operate = function (a,b, operator){
+    if (operator == 'add'){
+        return add(a,b);
+    }else if (operator == 'subtract'){
+        return subtract(a,b);
+    }else if (operator == 'multiply'){
+        return multiply(a,b);
+    }else if (operator == 'divide'){
+        return divide(a,b);
+    }
+     
+}
+
+console.log(operate(15,5,'divide'));
